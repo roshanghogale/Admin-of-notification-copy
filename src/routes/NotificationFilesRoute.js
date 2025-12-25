e7a6ea92f8a53b283d9f6610ae3fd720671ba16e
roshanghogale <126495884+roshanghogale@users.noreply.github.com>
2025-12-25 09:22:01 +0530

    new update story size

--- src/routes/NotificationFilesRoute.js
+++ src/routes/NotificationFilesRoute.js
@@ -20,7 +20,12 @@ const storage = multer.diskStorage({
   }
 });
 
-const upload = multer({ storage });
+const upload = multer({ 
+  storage,
+  limits: {
+    fileSize: 50 * 1024 * 1024 // 50MB limit
+  }
+});
 
 router.get('/', getAllNotificationFiles);
 router.post('/', upload.single('image'), createNotificationFile);
